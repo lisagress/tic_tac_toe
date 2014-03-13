1f1c28f7ed8302e8dffa2f3bc38fe03095853970
Lisa Gress <lisamgress@gmail.com>
2014-03-12 18:53:32 -0700

    Add ability to check for a win

--- main.js
+++ main.js
@@ -20,18 +20,57 @@ function displayMessage(player) {
 	$("#message").text(message);
 }
 
+function checkThreeSquares(squares) {
+	var square1 = squares[0];
+	var square2 = squares[1];
+	var square3 = squares[2];
+
+	if($(square1).hasClass("selected") && ($(square1).text() == $(square2).text() && $(square1).text() == $(square3).text())) {
+		return true;
+	} else {
+		return false;
+	};
+}
+
+function checkForWin() {	
+	var winningCombinations = [[".top.left", ".top.center", ".top.right"],
+								[".middle.left", ".middle.center", ".middle.right"],
+								[".bottom.left", ".bottom.center", ".bottom.right"],
+								[".top.left", ".middle.left", ".bottom.left"],
+								[".top.center", ".middle.center", ".bottom.center"],
+								[".top.right", ".middle.right", ".bottom.right"],
+								[".top.left", ".middle.center", ".bottom.right"],
+								[".top.right", ".middle.center", ".bottom.left"]]
+	for(i = 0; i < winningCombinations.length; i++) {
+		var winFound = checkThreeSquares(winningCombinations[i]);
+		if(winFound) {
+			return true;
+		};
+	}
+	return false;
+}
+
+
 $(document).ready(function() {
 	var player = currentPlayer();
 	displayMessage(player);
 
 	$(".square").click(function() {
 		selectSquare(this, player);
-		player = currentPlayer();
-		displayMessage(player);
+		var win = checkForWin();
+
+		if (win) {
+			var winMessage = "Player " + player + ", YOU WIN!!";
+			$("#message").text(winMessage);
+		} else {
+			player = currentPlayer();
+			displayMessage(player);
+		};
 	});
 
 	$(".new_game").click(function() {
 		$(".square").empty();
 		$(".square").removeClass("selected");
+		$("#message").empty();
 	})
 });
